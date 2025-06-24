855384753b0304b99f528d95c5b805ff85800060
Jester David <davidjesterbouy@gmail.com>
2025-06-24 21:17:09 +0800

    Updated no more double sub menu

--- library_extensions/__manifest__.py
+++ library_extensions/__manifest__.py
@@ -1,34 +1,22 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "library_extensions",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
-    'description': """
-Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
+    "name": "library_extensions",
+    "version": "0.1",
     'depends': ['base', 'library'],
-
-    # always loaded
+    "external_dependencies": {},
+    "author": "Odev Solutions",
+    "website": "https://www.odevsolutions.com",
+    "summary": """Library Category""",
+    "description": """
+        Library Category
+    """,
+    "category": "Extra Tools",
     'data': [
     'security/ir.model.access.csv',
     'views/library_book_views.xml',
     'views/library_book_category_views.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    "installable": True,
+    "application": False,
 }
+
 
